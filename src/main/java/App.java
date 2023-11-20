2d147ac9b3abc582f406c0a9e8a366531cbe1f3e
руслан каракян <ruslan.karakyan@yandex.ru>
2023-11-20 18:39:28 +0300

    проходит тест)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        boolean compareHelloWorld = bean == bean2;
+        boolean compareCat = catBean == catBean2;
+        System.out.println(compareHelloWorld);
+        System.out.println(compareCat);
     }
 }
